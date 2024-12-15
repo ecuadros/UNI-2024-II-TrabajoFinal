2c7eb1ee22e4933386e40236214058a33365efdf
Anthony Dolores-Narciso <tonynarciso2003@gmail.com>
2024-12-15 13:36:34 -0600

    menu

---
+++ menu.h
@@ -0,0 +1,19 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+#include "menu.h"
+
+using namespace std;
+
+void Menu()
+{
+    cout << "\n--- Mini Excel ---\n";
+    cout << "1. Mostrar hoja de calculo\n";
+    cout << "2. Editar celda\n";
+    cout << "3. Salir\n";
+    cout << "Selecciona una opcion: ";
+}
+
+
+#endif // MENU_H_
