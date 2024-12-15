41c5ebf0a4bd1c7e4dcf37bd36fcf63d5596fdae
Cristhyan Perez <pevicristhyan@gmail.com>
2024-12-15 16:07:56 -0500

    Move all the files to principal directory

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Node.h"
+#include "test.h"
+#include "Mini_Excel.h"
+
+int main() {
+    //std::cout << "Ejecutando pruebas de Node.h:\n" << std::endl;
+    //runTests();
+    runMini();
+
+    return 0;
+}
