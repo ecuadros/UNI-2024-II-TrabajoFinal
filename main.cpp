0ba56c30590547a1a8ce66b938c36b9c22f0c0a6
Anthony Dolores-Narciso <tonynarciso2003@gmail.com>
2024-12-15 14:32:02 -0500

    subiendo el proyecto

--- main.cpp
+++ main.cpp
@@ -1,1 +1,42 @@
-//hola
+//empezando el miniExcel
+#include <iostream>
+#include "menu.h"
+#include "editCelda.h"
+
+using namespace std;
+int main()
+{
+    int filas = 26, columnas = 7; //numero inicial de filas y columnas
+    HojaCalculo hoja(filas, columnas); //creando objeto
+
+    bool running = true;
+    while (running) {
+        Menu();
+
+        int opcion;
+        cin >> opcion;
+
+        switch (opcion) {
+        case 1:
+            cout << "\nContenido de la hoja:\n";
+            hoja.mostrar();
+            break;
+
+        case 2:
+            EditCelda(hoja);
+            break;
+
+        case 3:
+            running = false;
+            cout << "Saliendo del programa...\n";
+            break;
+
+        default:
+            cout << "Opcion no valida. Intentalo de nuevo.\n";
+        }
+    }
+
+    return 0;
+    return 0;
+
+}
